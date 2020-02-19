7898b4bcaa8266dcdc2b5ecc9b06f1235a4e5ced
Andres <bicho_suarez@hotmail.com>
2020-02-18 19:34:46 -0700

    TablaConstante created

---
+++ SqlParser/Tablas/CrearTablas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SqlParser.Simbolos;
+
+namespace SqlParser.Tablas
+{
+    class CrearTablas
+    {
+        public TablaDelimitador tablaD = new TablaDelimitador();
+        public TablaOperador tablaO = new TablaOperador();
+        public TablaRelacional tablaRel = new TablaRelacional();
+        public TablaReservada tablaRes = new TablaReservada();
+
+        public TablaConstante tablaC;
+
+        public CrearTablas(String[] palabras, String[] constantes, String[] identificadores,
+                           int[] nConstantes, int[] lIdentificador)
+        {
+            this.tablaC = new TablaConstante(constantes, nConstantes);
+        }
+
+    }
+}
---
+++ SqlParser/Tablas/TablaConstante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SqlParser.Simbolos;
+
+namespace SqlParser.Tablas
+{
+
+    class TablaConstante
+    {
+        public Constante[] palabras;
+
+        public TablaConstante(String[] constantes, int[] nConstantes)
+        {
+            this.palabras = new Constante[constantes.Length];
+
+            for (int x = 0; x < constantes.Length; x++)
+            {
+                this.palabras[x] = new Constante(constantes[x], nConstantes[x]);
+            }
+
+        }
+
+
+    }
+}
