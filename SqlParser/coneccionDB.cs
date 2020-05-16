ae2835697eb8197af7b47671d55f5a5095351787
Netroid2000 <56058353+Netroid2000@users.noreply.github.com>
2020-05-15 19:13:47 -0600

    conexion db 1/2

---
+++ SqlParser/coneccionDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SqlParser
+{
+    class coneccionDB
+    {
+        MySqlConnection conexionDB;
+        public coneccionDB() {
+            this.conexionDB = new MySqlConnection("DataBase = parse; Data Source = 127.0.0.1; Port = 3308");
+        }
+        private void coneccion() {
+        
+        }
+    }
+}
